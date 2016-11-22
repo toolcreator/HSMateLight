0ace3b4c0fbaf2d745479769ec413e6725fdb975
Sven Mattsen <sven@sven.cc>
2016-11-22 11:22:06 +0100

    random monadic

---
+++ MainRandomMonad.hs
@@ -0,0 +1,23 @@
+import MateLight
+import ListFrame
+
+import Data.Maybe
+import qualified Network.Socket as Sock
+import Control.Monad.Trans
+import System.Random
+import Control.Monad
+import Data.Word
+
+eventTest :: (Int, Int) -> [EventT] -> MateMonad ListFrame s IO ListFrame
+eventTest dim events = liftIO frame >>= return . ListFrame
+  where
+  line = replicateM (fst dim) randomPixel
+  frame = replicateM (snd dim) line
+  randomPixel = do
+    r <- randomIO :: IO Word8
+    g <- randomIO :: IO Word8
+    b <- randomIO :: IO Word8
+    return $ Pixel r g b
+
+main :: IO ()
+main = let dim = (30,12) in Sock.withSocketsDo $ runMateM (Config (fromJust $ parseAddress "127.0.0.1") 1337 dim (Just 25000) True []) (eventTest dim) undefined
